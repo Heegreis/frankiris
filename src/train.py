5ccab2f33daf3160a751c11e87bc43aabab60404
Heegreis <run3113214@gmail.com>
2021-04-18 22:51:45 +0800

    upgrade hydra and add logger

--- src/train.py
+++ src/train.py
@@ -7,7 +7,14 @@ def train(cfg):
 
     # Init Lightning model
     module = hydra.utils.instantiate(cfg.module)
+
+    # Init Lightning loggers
+    if "logger" in cfg:
+        logger = []
+        for _, loggerCfg in cfg.logger.items():
+            logger.append(hydra.utils.instantiate(loggerCfg))
+            print(logger[0])
     
     # init trainer
-    trainer = hydra.utils.instantiate(cfg.trainer)
+    trainer = hydra.utils.instantiate(cfg.trainer, logger=logger)
     trainer.fit(module, dataModule)
