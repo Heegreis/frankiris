58437db5f63abbfd844fb50c662ccf2d074f05a9
Heegreis <run3113214@gmail.com>
2021-05-23 15:46:58 +0800

    transform instance use eval

--- src/train.py
+++ src/train.py
@@ -10,23 +10,23 @@ def train(cfg):
         transform = None
 
     # Init Lightning datamodule
-    dataModule = hydra.utils.instantiate(cfg.dataModule, transform=transform)
+    dataModule = hydra.utils.instantiate(cfg['dataModule'], transform=transform)
 
     # # Init Lightning model
-    module = hydra.utils.instantiate(cfg.module)
+    module = hydra.utils.instantiate(cfg['module'])
 
     # Init Lightning loggers
     if "logger" in cfg:
         logger = []
-        for _, logger_cfg in cfg.logger.items():
+        for _, logger_cfg in cfg['logger'].items():
             logger.append(hydra.utils.instantiate(logger_cfg))
     
     # Init Lightning callbacks
     if "callbacks" in cfg:
         callbacks = []
-        for _, callbacks_cfg in cfg.callbacks.items():
+        for _, callbacks_cfg in cfg['callbacks'].items():
             callbacks.append(hydra.utils.instantiate(callbacks_cfg))
 
     # Init Lightning trainer
-    trainer = hydra.utils.instantiate(cfg.trainer, logger=logger, callbacks=callbacks)
+    trainer = hydra.utils.instantiate(cfg['trainer'], logger=logger, callbacks=callbacks)
     trainer.fit(module, dataModule)
--- src/utils/composeTransform.py
+++ src/utils/composeTransform.py
@@ -1,8 +1,22 @@
 import hydra
 from src.utils.cfgfix import fix_tuple
+import torchvision
+import torchaudio
 from torchvision import transforms
 import torch.nn as nn
 
+
+def instanceTransform(transform_cfg):
+    target = ''
+    p = ''
+    for key, value in transform_cfg.items():
+        if key == '_target_':
+          target = value
+        else:
+            p = p + key + '=' + value + ','
+    p = p[:-1]
+    cmd = eval(target + '(' + p + ')')
+    return cmd
 
 def getTransform(cfg):
     stages = ['train', 'val', 'test']
@@ -11,12 +25,12 @@ def getTransform(cfg):
         transform[stage] = {}
         audio_transform_list = []
         vision_transform_list = []
-        for transform_cfg in cfg.transforms[stage]:
-            fix_tuple(transform_cfg)
+        for transform_cfg in cfg['transforms'][stage]:
+            # fix_tuple(transform_cfg)
             if transform_cfg['_target_'].split('.')[0] == 'torchaudio':
-                audio_transform_list.append(hydra.utils.instantiate(transform_cfg))
+                audio_transform_list.append(instanceTransform(transform_cfg))
             elif transform_cfg['_target_'].split('.')[0] == 'torchvision':
-                vision_transform_list.append(hydra.utils.instantiate(transform_cfg))
+                vision_transform_list.append(instanceTransform(transform_cfg))
 
         transform[stage]['audio'] = nn.Sequential(*audio_transform_list)
         transform[stage]['vision'] = transforms.Compose(vision_transform_list)
